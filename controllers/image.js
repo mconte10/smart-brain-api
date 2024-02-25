0b042fbdae03e338ed21568f48b8775fce4fbeb0
Maurizio Conte <131488898+mconte10@users.noreply.github.com>
2024-02-25 15:40:10 -0500

    Add files via upload

---
+++ controllers/image.js
@@ -0,0 +1,53 @@
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key " + 'd758ae04279d49738ac553578202ffa0');
+
+const handleApiCall = (req, res) => {
+    stub.PostModelOutputs(
+        {
+            user_app_id: {
+                "user_id": 'mconte10',
+                "app_id": 'my-first-application-qz5oj8'
+            },
+            model_id: "face-detection",
+            inputs: [{data: {image: {url: req.body.input}}}]
+        },
+
+        metadata,
+        (err, response) => {
+            if (err) {
+                console.log("Error: " + err);
+                return;
+            }
+    
+            if (response.status.code !== 10000) {
+                console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+                return;
+            }
+    
+            console.log("Predicted concepts, with confidence values:")
+            for (const c of response.outputs[0].data.concepts) {
+                console.log(c.name + ": " + c.value);
+            }
+            res.json(response)
+        }
+    );
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('Unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+};
