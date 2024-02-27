9d8bd018a258ca5dcc818322d6e3c99faa6c676e
mconte10 <mochoconte@gmail.com>
2024-02-26 20:55:47 -0500

    fixing issue

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res, db)})
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.listen(PORT || 3000, () => {
     console.log(`app is running on port ${PORT}`);
