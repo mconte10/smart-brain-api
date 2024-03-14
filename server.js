b579513bad9b00115f96c00d3263f020157a5142
mconte10 <mochoconte@gmail.com>
2024-03-14 09:06:21 -0400

    updating database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-horizontal-57300',
       port : 5432,
       user : 'postgres',
       password : 'Future-Engineer3',
