429c779e61a7e43ed7a519a90c83f376f78fda07
mconte10 <mochoconte@gmail.com>
2024-03-14 10:23:52 -0400

    fixed db connection

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-horizontal-57300',
+      host : '127.0.0.1',
       port : 5432,
       user : 'postgres',
       password : 'Future-Engineer3',
